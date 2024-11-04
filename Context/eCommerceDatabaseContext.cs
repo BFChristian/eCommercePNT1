e21bb46bf680d5083e069a7dafcf4a2b63a9c71a
Christian <b.christian.fuentes@gmail.com>
2024-11-04 02:25:21 -0300

    Add project files.

---
+++ Context/eCommerceDatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using eCommerce.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace eCommerce.Context
+{
+    public class eCommerceDatabaseContext : DbContext
+    {
+        public eCommerceDatabaseContext(DbContextOptions<eCommerceDatabaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
---
+++ Models/Producto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Models
+{
+    public class Producto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public double Precio { get; set; }
+        public int Stock { get; set; }
+
+        public Producto(int id, string nombre, string descripcion, double precio, int stock)
+        {
+            Id = id;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Precio = precio;
+            Stock = stock;
+        }
+    }
+}
